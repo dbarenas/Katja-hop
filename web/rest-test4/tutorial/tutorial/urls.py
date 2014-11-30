a51aa6b51ed946f2cfa1f025f19753bab4ad8968
David Arenas <dabuiar@gmail.com>
2014-12-01 00:08:08 +0100

    rest 5 soyu

--- web/rest-test4/tutorial/tutorial/urls.py
+++ web/rest-test4/tutorial/tutorial/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
-urlpatterns = patterns('',
- 
-    url(r'^', include('snippets.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    
-)
+urlpatterns = [
+    url(r'^snippets/$', views.snippet_list),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
